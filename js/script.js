2948f1370d602d3aca9a486ce32a07478df586d4
Almazmin Minhaz <68338495+AlmazminMinhaz@users.noreply.github.com>
2020-10-23 21:09:15 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function textChange(){
+	document.getElementById('h3').innerHTML="My First Js Code";
+}
+// textChange();
+
+function textChange(){
+	document.getElementById('h4').innerHTML="My First Js Code";
+}
